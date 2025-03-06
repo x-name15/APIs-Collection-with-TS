8873d7685213cb1181a15ac21a88f3f160458391
Felix Manrique | ElFlez | -xd- | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2025-03-06 13:05:17 +0100

    Update movie.model.ts

--- movie.model.ts
+++ movie.model.ts
@@ -4,14 +4,13 @@ export interface IMovie extends Document {
   title: string;
   director: string;
   year: number;
-  genre: string;
 }
 
 const MovieSchema: Schema = new Schema({
   title: { type: String, required: true },
   director: { type: String, required: true },
   year: { type: Number, required: true },
-  genre: { type: String, required: true },
 });
 
 export default mongoose.model<IMovie>("Movie", MovieSchema);
+

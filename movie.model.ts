a99f6f27b46586f1b5c37972ddc576ebbaed5589
Felix Manrique | ElFlez | -xd- | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2025-03-07 10:19:04 +0100

    Update movie.model.ts

--- movie.model.ts
+++ movie.model.ts
@@ -1,16 +1,18 @@
 import mongoose, { Schema, Document } from "mongoose";
+import Genre from "./genre.model"; // Importar el modelo Genre
 
-export interface IMovie extends Document {
-  title: string;
-  director: string;
-  year: number;
+interface IMovie extends Document {
+    title: string;
+    director: string;
+    year: number;
+    genres: mongoose.Types.ObjectId[]; // Arreglo de ObjectId que referencia a los géneros
 }
 
-const MovieSchema: Schema = new Schema({
-  title: { type: String, required: true },
-  director: { type: String, required: true },
-  year: { type: Number, required: true },
+const MovieSchema = new Schema({
+    title: { type: String, required: true },
+    director: { type: String, required: true },
+    year: { type: Number, required: true },
+    genres: [{ type: mongoose.Schema.Types.ObjectId, ref: "Genre" }] // Referencia a los géneros
 });
 
 export default mongoose.model<IMovie>("Movie", MovieSchema);
-

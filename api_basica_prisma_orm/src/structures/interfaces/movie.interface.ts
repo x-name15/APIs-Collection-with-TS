78e76d998ef01f74949536bbafec799dd5884ff0
Felix Manrique | Flez | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2025-09-24 09:52:56 +0200

    Delete api_basica_prisma_orm directory

--- api_basica_prisma_orm/src/structures/interfaces/movie.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Genre } from './genre.interface.js';
-
-export interface Movie {
-  id: string;
-  title: string;
-  director: string;
-  year: number;
-  genres: Genre[]; // Tipamos los géneros "populados" que vamos a devolver
-}

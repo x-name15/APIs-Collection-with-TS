d264d76237e0ba7e7693e4be099b049e963d557c
Felix Manrique | Flez | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2025-09-24 09:32:10 +0200

    Delete src directory

--- src/database/models/genre.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, Document } from "mongoose";
-import GenreSchema from "../schemas/genre.schema";
-import { IGenre } from "../../structures/interfaces/genre.interface";
-
-interface IGenreDocument extends IGenre, Document {}
-
-const Genre = model<IGenreDocument>("Genre", GenreSchema);
-
-export default Genre;

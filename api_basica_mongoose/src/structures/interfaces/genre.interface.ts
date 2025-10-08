9a6c24663b8f0a86d2df5d4b00db867b6094b22f
Felix Manrique <felix.manrique@ct.uneatlantico.es>
2025-10-08 08:13:55 +0200

    fix: removed the other API collection from this branch

--- api_basica_mongoose/src/structures/interfaces/genre.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IGenre {
-    name: string;
-  }
-  
--- api_basica_mongoose/src/structures/interfaces/movie.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from "mongoose";
-import { IGenre } from "./genre.interface";
-
-export interface IMovie {
-  title: string;
-  director: string;
-  year: number;
-  genres: Types.ObjectId[] | IGenre[];
-}
